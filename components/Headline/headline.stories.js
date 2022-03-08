63d38011835486b57b0ea218aca1cf1b57fa671b
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-03-08 21:37:43 +0100

    Try build components

---
+++ components/Headline/headline.stories.js
@@ -0,0 +1,8 @@
+import "./headline.css";
+
+export default {
+    title: "Components/Headline",
+};
+export const h1 = () => '<h1>Hello World</h1>';
+export const h2 = () => '<h2>Hello World</h2>';
+export const h3 = () => '<h3>Hello World</h3>';
---
+++ components/Social-links/social-link.stories.js
@@ -0,0 +1,10 @@
+import "./social-link.css";
+
+export default {
+  title: "Components/Social-links",
+};
+export const Default = () =>
+    `<ul class="social-link">
+    <li class="social-link-item">Twitter</li>
+    <li class="social-link-item">Github</li>
+    </ul>`
